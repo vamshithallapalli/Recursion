530fe7682403b7ed68b9c3c0ae881983bced2e55
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2024-03-12 15:42:31 +0530

    Add files via upload

---
+++ 3Factorial.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int factorial(int x){
+    if(x==1){
+        return 1;
+    }
+    else{
+        return x*factorial(x-1);
+    }
+}
+int main(){
+    int x;
+    cin>>x;
+    cout<<factorial(x)<<endl;
+
+return 0;
+}
